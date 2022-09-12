b4eaf1cb4ee02a218e927a106d5276aab6220864
Erik Peterson <erik@thebibleway.com>
2022-09-12 12:03:10 -0500

    Added code during lecture.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 #include "TestClass.h"
 
+void myAtExitFunc() {
+    std::cout << "myAtExitFunc" << std::endl;
+}
+
 void func2(std::shared_ptr<TestClass> sharedPtr) {
     sharedPtr->shareWisdom();
 }
 
 void func() {
+    /*int* myInt = (int*) malloc(sizeof(int));
+    free(myInt);
+    myInt = nullptr;
+    free(myInt);*/
     std::shared_ptr<TestClass> sharedPtr = std::make_shared<TestClass>();
     sharedPtr->shareWisdom();
     func2(sharedPtr);
@@ -13,6 +21,7 @@ void func() {
 }
 
 int main() {
+    //atexit(myAtExitFunc);
     std::cout << "Hello!" << std::endl;
     func();
     std::cout << "Goodbye!" << std::endl;
